d07ed2e58e1212d8b627193b8f930a82c3b3f2e2
Pavan Kavade <pavankavade@users.noreply.github.com>
2026-09-07 16:09:11 +0530

    Update site canonical and OpenGraph URLs to live Cloudflare Pages domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://teaandtoastblog.com',
+  site: 'https://teaandtoastblog.pages.dev',
   integrations: [
     tailwind({
       applyBaseStyles: false,
